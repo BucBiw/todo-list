033764e20006a2354bcd381a4854f2657be7b4be
BucBiw <thibad177@gmail.com>
2020-09-23 12:51:37 +0700

    add bootstrap and expressJS

--- todo-list-fnt/src/Home.js
+++ todo-list-fnt/src/Home.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function Home() {
-  return (
-    <div className="Home">
-      <header className="Home-header">
-        <h1>todo-List (Home)</h1>
-      </header>
-    </div>
-  );
+    return (
+        <div className="Home">
+            <header className="Home-header">
+                <h1>todo-List (Home)</h1>
+            </header>
+            <Button variant="primary">Log In</Button>
+            <Button variant="light">Sign Up</Button>
+        </div>
+    );
 }
 
 export default Home;
