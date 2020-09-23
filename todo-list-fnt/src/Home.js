1f64979c02e9ce678668fd33297baae8b6376c18
BucBiw <thibad177@gmail.com>
2020-09-23 12:26:51 +0700

    Add Home Page

---
+++ todo-list-fnt/src/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="Home">
+      <header className="Home-header">
+        <h1>todo-List (Home)</h1>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
