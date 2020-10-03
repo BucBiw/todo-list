39f66a97c4d7c3d78b7f182db76ae0320b59c006
BucBiw <thibad177@gmail.com>
2020-10-03 17:57:07 +0700

    clear code in home page

--- todo-list-fnt/src/Home.Component/Home.js
+++ todo-list-fnt/src/Home.Component/Home.js
@@ -1,39 +1,25 @@
 import React, { useEffect } from 'react';
-import FacebookLogin from 'react-facebook-login';
-import Button from 'react-bootstrap/Button';
-import { BrowserRouter as Router } from "react-router-dom";
+import { Row, Button, Col } from 'react-bootstrap';
 
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Axios from 'axios';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import App from '../App.Component/App';
 
 
+
 function Home() {
-    const signUserIn = async (response) => {
-        console.log('Res---->', response);
-        const { name, email, accessToken, userID } = response
-        const user = { name, email, accessToken, userID: userID }
-
-        await Axios({
-            method: 'post',
-            url: 'http://localhost:4000/signin/facebook',
-            data: {
-                user,
-            }
-        });
-    }
-
     return (
         <div className='Home'>
-            <div>
-                <FacebookLogin
-                    appId='2107368362741206'
-                    fields='name, email'
-                    scope='public_profile, email'
-                    callback={signUserIn} 
-                />
-            </div>
+            <Row>
+                <Col>
+                    <h1>ToDoList</h1>
+                </Col>
+                <Col>
+                    <
+                </Col>
+            </Row>
+            <h1>ToDoList</h1>
         </div>
     );
 }
